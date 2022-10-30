619ed39a7c7984b6e41131f5abea5affbec938b6
chifou <88934786+Chifouuu@users.noreply.github.com>
2022-10-30 11:21:31 +0100

    Update Reference.java

--- src/main/java/com/chifou/decotools/utils/Reference.java
+++ src/main/java/com/chifou/decotools/utils/Reference.java
@@ -5,4 +5,6 @@ public class Reference {
     public static final String MOD_ID = "decotools";
     public static final String MOD_AUTHOR = "Chifou";
     public static final String MOD_VERSION = "1.0";
+
+    //msg
 }
